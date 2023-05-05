6d5f4a43a99d03da0e95b28e1cfeadfda15f3c90
masumeh <khanimasume88@gmail.com>
2023-05-05 21:11:53 +0430

    ExpenseFilter component done

--- src/components/Expensefilter.jsx
+++ src/components/Expensefilter.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-
-const Expensefilter = () => {
+const Expensefilter = ({ onSelectCategory }) => {
   return (
-    <div>
+    <div className="m-3 lg:w-2/5 sm:w-fit">
       <label
-        for="countries"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="categories"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white sm:inline-block sm:w-1/5 md:w-1/6 lg:w-1/8"
       >
-        Select your country
+        All categories
       </label>
       <select
-        id="countries"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        onChange={(e) => onSelectCategory(e.target.value)}
+        id="categories"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:w-3/4 md:w-4/5 lg:w-5/6 sm:text-xs"
       >
-        <option>United States</option>
-        <option>Canada</option>
-        <option>France</option>
-        <option>Germany</option>
+        <option></option>
+        <option value="Groceries">Groceries</option>
+        <option value="Utilities">Utilities</option>
+        <option value="Entertainment">Entertainment</option>
       </select>
     </div>
   );
