6563dcb2b2665c18de8d4371a028bef702557777
masumeh <khanimasume88@gmail.com>
2023-05-06 07:12:22 +0430

    add components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 
+import { useState } from "react";
+import Expensefilter from "./components/Expensefilter";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 function App() {
+  const [expenses, setExpenses] = useState([
+    { description: "milk", id: 1, amount: 10, category: "Groceries" },
+    { description: "sugur", id: 2, amount: 20, category: "Utilities" },
+    { description: "car", id: 3, amount: 1000, category: "Entertainment" },
+    { description: "Painting", id: 4, amount: 10, category: "Groceries" },
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category == selectedCategory)
+    : expenses;
   return (
-    <div className="App">
+    <div className="py-10 flex flex-col lg:max-w-screen-lg mx-auto ">
+      <ExpenseForm
+        onSubmit={(expense) => {
+          setExpenses([...expenses, { ...expense, id: expenses.length + 1 }]);
+          console.log(expenses);
+        }}
+      />
+      <Expensefilter
+        onSelectCategory={(category) => setSelectedCategory(category)}
+      />
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
